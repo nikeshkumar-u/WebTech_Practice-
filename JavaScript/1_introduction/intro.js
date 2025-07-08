c229c142d64aed2514f06204a76569a80dfc0bc0
nikeshkumar-u <udhayanikesh001@gmail.com>
2025-07-08 21:52:48 +0530

    Js practise

---
+++ JavaScript/1_introduction/intro.js
@@ -0,0 +1,56 @@
+console.log("I'm external JavaScript");
+
+
+// how many ways we can declare variable?
+//   three ways
+//        1.let
+let My_name
+
+My_name="Nikish"
+
+// let My_name (Re-declaretion is not possible)
+
+let myName= "NK"  
+
+console.log(My_name);
+console.log(myName);
+
+let num = "3214589"
+console.log(num);
+
+//        2.var
+var user = "Matesh"
+console.log(user);
+var user
+  user="bvbv"
+  console.log(user);
+//   var (re-declaretion is possible)
+  
+
+//        3.const
+     const number = "948746336"
+
+    //  const (re-initialization is not possible and re-declaretion also not possible and initialization and declaretion in same line)
+
+
+// ! data type
+// !  primitive
+//? 1) number
+
+   let id = 123
+   let sal = 10000000.00
+
+// ! typeof
+    // typeof operator is used to know the datatype of the variable
+
+    console.log(typeof id);
+    
+//? 2) string 
+    let emp1 = "kamal"
+    let emp2 = 'sam'
+    let emp3 = `saleem`
+
+    console.log(typeof emp3);
+
+    // ! datatype(undefined,boolean,string,)
+    
---
+++ JavaScript/2_string/string.js
@@ -0,0 +1,29 @@
+let str1 = `nikeshkumar`
+let str2 = `mathesh`
+let str3 = `kishore`
+
+console.log(str1)
+console.log(str2)
+console.log(str3)
+
+
+console.log(str1.length);
+
+console.log(str1.toUpperCase())
+str1.toUpperCase()
+// original string is not modified it's returning new string
+console.log(str1);
+
+let b=str2.toLowerCase()
+console.log(b);
+let c=str1.indexOf(`s`)
+console.log(c);
+let c1 = str1.lastIndexOf(`k`)
+console.log(c1);
+let msg = `heloo  mat`
+console.log(msg.includes(`mat`));
+
+
+
+
+
